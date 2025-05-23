e6b1a729f9870c299ff0c29a615341f4c2a82c85
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2025-05-23 11:12:16 +0800

    initial structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use tfhe::{ ConfigBuilder, generate_keys, set_server_key, FheUint8 };
+use tfhe::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+	let config = ConfigBuilder::default().build();
+
+	// client-side
+	let (client_key, server_key) = generate_keys(config);
+
+	let clear_a = 27u8;
+	let clear_b = 128u8;
+
+	let enc_a = FheUint8::encrypt(clear_a, &client_key);
+	let enc_b = FheUint8::encrypt(clear_b, &client_key);
+
+	// server side
+	set_server_key(server_key);
+	let enc_res = enc_a + enc_b;
+
+	// client-side
+	let res: u8 = enc_res.decrypt(&client_key);
+
+	assert_eq!(res, clear_a + clear_b);
 }
