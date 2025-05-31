48ee5f528662b09420e2f809f092365d1c710047
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2025-05-31 19:42:28 +0800

    refactor test cases (#6)

--- src/tests/plain_dark_pool.rs
+++ src/tests/plain_dark_pool.rs
@@ -27,3 +27,26 @@ fn handle_simple_case() {
 	assert_eq!(b_fulfilled, vec![(1, 0), (2, 30), (3, 80)]);
 	assert_eq!(s_fulfilled, vec![(3, 40), (3, 40), (2, 30)]);
 }
+
+#[test]
+fn handle_complex_case() {
+	let b_orders: Vec<(u32, u32)> = (0..500).map(|i| (i % 50 + 1, i % 100 + 1)).collect();
+
+	let s_orders: Vec<(u32, u32)> = (0..400).map(|i| (i % 50 + 1, i % 100 + 1)).collect();
+
+	let expected_b_fulfilled: Vec<(u32, u32)> = [
+		(0..400).map(|i| (i % 50 + 1, i % 100 + 1)).collect::<Vec<_>>(),
+		(400..500).map(|i| (i % 50 + 1, 0)).collect::<Vec<_>>(),
+	]
+	.concat();
+
+	let expected_s_fulfilled = s_orders.clone();
+
+	// server-side processing
+	let dp = PlainDarkPool::new();
+	let (b_fulfilled, s_fulfilled) = dp.volume_matching(b_orders, s_orders);
+
+	// client-side
+	assert_eq!(b_fulfilled, expected_b_fulfilled);
+	assert_eq!(s_fulfilled, expected_s_fulfilled);
+}
